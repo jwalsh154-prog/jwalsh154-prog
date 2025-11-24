2f230461e97f3005bca89a823496e87f8312997e
Lara Moussa <lmouss1@students.towson.edu>
2025-11-24 02:36:52 -0500

    added notification, payment and help setup

--- app/src/main/java/com/example/newbegdinnings/PaymentActivity.kt
+++ app/src/main/java/com/example/newbegdinnings/PaymentActivity.kt
@@ -1,18 +1,27 @@
 package com.example.newbegdinnings
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-class HelpActivity : AppCompatActivity() {
+class PaymentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_help)
+        setContentView(R.layout.activity_payment)
         val main = findViewById<android.view.View>(R.id.main)
         ViewCompat.setOnApplyWindowInsetsListener(main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets }
+            insets
+        }
+        val btnConfirm = findViewById<Button>(R.id.btnConfirmPayment)
+        btnConfirm.setOnClickListener {
+            Toast.makeText(this, "Payment confirmed for prototype", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
+
